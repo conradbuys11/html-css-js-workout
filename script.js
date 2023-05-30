2c03197ee5b1fc12485ea2fcae85069569f732ed
conradbuys11 <conradbuys11@gmail.com>
2023-05-30 16:46:43 -0400

    feat: logic to switch border between 4 colors

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello from Nerdbord!')
+let box = document.querySelector("div.box");
+let colors = ["red", "yellow", "blue", "green"];
+let curColor = 0;
+
+const colorSwitcher = () => {
+  box.style.borderColor = colors[curColor];
+  if (curColor >= colors.length - 1) {
+    curColor = 0;
+  } else {
+    curColor += 1;
+  }
+};
+
+setInterval(colorSwitcher, 3000);
